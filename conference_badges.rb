eb770dfb6c66fdaedd990b37aef409fb98b6fe7a
Joshua Williams <j.o-williams@outlook.com>
2020-04-04 14:53:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,4 +13,5 @@ end
 def assign_rooms(array)
   array.each_with_index do |assigned, index|
     array[array.index(assigned)] = "Hello, #{assigned}! You'll be assigned to room #{index+1}!"
+  end
   
